d8747eab589dea7cb49ad20350fe409c646b64d8
Pedro de Oliveira <falsovsky@gmail.com>
2017-04-06 19:52:40 +0100

    Fix bug and use unix newlines

--- aup.pl
+++ aup.pl
@@ -23,19 +23,18 @@ sub my_aup {
 
     if ( $message =~ m/-who (\S+)/ ) {
         $who = $b . $1 . $b . " - ";
-        $message =~ s/-who \S+//g; 
+        $message =~ s/-who \S+//g;
     }
 
     if ($message eq "") {
         $message = "Nao saber usar o script";
     }
 
+    $message =~ s/^\s+|\s+$//g;
+
     my $encoded_message = $message;
     $encoded_message =~ s/([^A-Za-z0-9 ]+)//g;
     $encoded_message =~ s/ /%20/g;
-
-    $message =~ s/^\s+|\s+$//g;
-
 
     my $aup = "/!\\ Aviso: $who${c}13$message$c é uma violação da AUP da DeadBSD (consulte em: http://aup.deadbsd.org/$encoded_message ) e pode incorrer em sansões graves. Lembre-se de utilizar este serviço como um privilégio e nunca como um direito!";
 
